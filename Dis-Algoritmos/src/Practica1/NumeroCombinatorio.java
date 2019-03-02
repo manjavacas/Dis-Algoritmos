684acf1e07f10bdf4c9b0b4944b2bfba41ddedad
Antonio <Antonio@Antonio>
2019-03-02 09:28:28 +0100

    Commit

--- Dis-Algoritmos/src/Practica1/NumeroCombinatorio.java
+++ Dis-Algoritmos/src/Practica1/NumeroCombinatorio.java
@@ -1,14 +1,17 @@
 package Practica1;
 
+import java.util.Stack;
+
 public class NumeroCombinatorio {
 
 	public static void main(String[] args) {
 
-		int n = 50, k = 20;
+		// Debe cumplirse que n >= k
+		int n = 6, k = 2;
 
 		System.out.println(combinatorioIterativo(n, k));
 		System.out.println(combinatorioRecursivo(n, k));
-		// System.out.println(combinatorioPila(n, k));
+		System.out.println(combinatorioPila(n, k));
 	}
 
 	// Calcular factorial de un numero
@@ -42,7 +45,63 @@ public class NumeroCombinatorio {
 
 	// Calcular numero combinatorio mediante pilas
 	private static int combinatorioPila(int n, int k) {
-		return 1;
+		Stack<Integer> pilaN = new Stack<Integer>();
+		Stack<Integer> pilaK = new Stack<Integer>();
+		Stack<Integer> pilaL = new Stack<Integer>();
+		Stack<Integer> pilaSol = new Stack<Integer>();
+
+		pilaN.push(n);
+		pilaK.push(k);
+		pilaL.push(1);
+		pilaSol.push(0);
+
+		return combinatorio(pilaN, pilaK, pilaL, pilaSol);
+	}
+
+	private static int combinatorio(Stack<Integer> pilaN, Stack<Integer> pilaK, Stack<Integer> pilaL,
+			Stack<Integer> pilaRes) {
+
+		int sol = -1;
+		while (!pilaN.empty() || !pilaK.empty()) {
+			while (pilaN.peek() > 0 && pilaK.peek() > 0) {
+				switch (pilaL.peek()) {
+				// Primera llamada
+				case 1:
+					pilaN.push(pilaN.peek() - 1);
+					pilaK.push(pilaK.peek() - 1);
+					break;
+				// Segunda llamada
+				case 2:
+					pilaN.push(pilaN.peek() - 1);
+					pilaK.push(pilaK.peek());
+					break;
+				}
+				pilaL.push(1);
+				
+				if (pilaN.peek() == 0) {
+					pilaRes.push(1);
+				} else if (pilaK.peek() == 0) {
+					pilaRes.push(0);
+				} else {
+					pilaRes.push(0);
+				}
+			}
+
+			pilaN.pop();
+			pilaL.pop();
+			sol = pilaRes.pop();
+
+			if (!pilaN.empty()) {
+				pilaL.push(pilaL.pop() + 1);
+				pilaRes.push(pilaRes.pop() + sol);
+			}
+
+			if (sol == 0)
+				sol = 1;
+			return sol;
+		}
+
+		return 0;
 	}
 
 }
